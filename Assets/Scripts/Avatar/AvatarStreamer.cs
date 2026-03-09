079befd9b50efc895104e2d73447c323f3495fba
Rodrigo Hervada Llahosa <hllrodrigo@gmail.com>
2026-03-09 12:12:42 +0100

    Add avatar and lip-sync
    Add Avatar assets (GLB, render texture, test audio), update Main Scene to include lip-sync-related GameObjects/components (LipSync, CameraAvatar, AvatarStreamer). Update project packages to include gltfast and uLipSync.

---
+++ Assets/Scripts/Avatar/AvatarStreamer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AvatarStreamer : MonoBehaviour
+{
+    public RenderTexture renderTexture;
+    public int width = 512;
+    public int height = 512;
+
+    public string mobileIP = "192.168.1.140";
+    public int port = 5000;
+}
