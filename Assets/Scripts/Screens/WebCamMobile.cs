ad90a4a70d1f3f7cb31a4b3571558f4634f5730c
Rodrigo Hervada Llahosa <hllrodrigo@gmail.com>
2026-03-23 13:40:36 +0100

    Reorganize assets and update scene
    Reorganize assets and update scene

--- Assets/Scripts/Screens/WebCamMobile.cs
+++ Assets/Scripts/Screens/WebCamMobile.cs
@@ -55,6 +55,7 @@ public class WebCamMobile : MonoBehaviour
         if (newFrame)
         {
             cameraTexture.LoadImage(latestFrame);
+            cameraTexture.Apply();
             rend.material.mainTexture = cameraTexture;
             newFrame = false;
         }
